b0870d9f7670a3b5b4040fcb841f7138b13c5535
JEAN Jonathan <jonathan.jean@epitech.eu>
2017-02-19 13:16:29 +0100

    Removing Adsense js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -155,15 +155,6 @@
 <script src="{{ asset('assets/js/dashboard.js') }}"></script>
 <script src="{{ asset('assets/js/cookie.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
-
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-4004111135039238",
-        enable_page_level_ads: true
-    });
-</script>
-
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
